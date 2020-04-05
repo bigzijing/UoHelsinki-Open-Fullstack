1b7e4f88bdbaec026e7aea262386a282c09bf06c
Zi Jing <bigzijing@gmail.com>
2020-04-05 21:25:40 +0800

    First commit

---
+++ 1/submission/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+        ]
+    }
+
+    const Content = ({course}) => {
+        return (
+            <div>
+                <h1>{course.name}</h1>
+
+                <p>{course.parts[0].name} {course.parts[0].exercises}</p>
+                <p>{course.parts[1].name} {course.parts[1].exercises}</p>
+                <p>{course.parts[2].name} {course.parts[2].exercises}</p>
+
+                <p>Total exercises {course.parts[0].exercises+course.parts[0].exercises+course.parts[0].exercises}</p>
+            </div>
+        )
+    }
+    
+        return (
+        <div>
+            <Content course={course} />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
